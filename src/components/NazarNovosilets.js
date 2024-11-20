c7791d6487f92d933e5320e766a89fd97284e558
NazarNovosilets <nazar.novosilets100@gmail.com>
2024-11-20 13:41:21 +0200

    change strings

--- src/components/NazarNovosilets.js
+++ src/components/NazarNovosilets.js
@@ -2,7 +2,7 @@ import React from "react";
 import Body from "./Body";
 
 const NazarNovosilets = () => {
-  return <Body name="Nazar Novosilets" position="UA\UX Designer" photoSrc="avatar-default.png" />;
+  return <Body name="Nazar Novosilets" position="UI\UX Designer" photoSrc="avatar-default.png" />;
 };
 
 export default NazarNovosilets;
