c2b2554fdf77b6ba95f56de1d876c461e2d73126
Sansanych122 <smikoleksandr7@gmail.com>
2024-11-20 13:57:30 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import OleksanrSmyk from "./components/OleksandrSmyk";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <div className="body-container">
+      <OleksanrSmyk />
+    </div>
     </div>
   );
 }
