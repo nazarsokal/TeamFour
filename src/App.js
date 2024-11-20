a78b9f72c470cc09207240ae4b07f7d3db54d1dc
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2024-11-20 12:00:00 +0100

    New Person Displayed
    Adding new Person displaying on the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import NazarSok from "./components/NazarSok";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <NazarSok />
     </div>
   );
 }
