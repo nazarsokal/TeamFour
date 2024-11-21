62b63e5a53c0653b587e17aa191d887349c4efab
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2024-11-21 12:35:36 +0100

    New Photo Merge Commit

--- src/components/NazarSok.js
+++ src/components/NazarSok.js
@@ -2,7 +2,7 @@ import React from "react";
 import Body from "./Body";
 
 const NazarSok = () => {
-  return <Body name="Nazar" position="Full-Stack developer" photoSrc="imageLLL.png" />;
+  return <Body name="Nazar" position="Full-Stack developer" photoSrc="avatar-default.png" />;
 };
 
 export default NazarSok;
